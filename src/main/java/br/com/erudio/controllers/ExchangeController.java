8e545a1f93e9a7cd93147c4b5b514d6754ba1c84
Gabriel Dellatore E. <dellatoregabriel8@gmail.com>
2026-07-20 21:36:20 -0300

    removing Controller Mocks

--- src/main/java/br/com/erudio/controllers/ExchangeController.java
+++ src/main/java/br/com/erudio/controllers/ExchangeController.java
@@ -2,6 +2,7 @@ package br.com.erudio.controllers;
 
 import br.com.erudio.environment.InstanceInformationService;
 import br.com.erudio.models.Exchange;
+import br.com.erudio.repositories.ExchangeRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,21 +16,24 @@ import java.math.BigDecimal;
 public class ExchangeController {
 
     private final InstanceInformationService instanceInformationService;
+    private final ExchangeRepository repository;
 
-    public ExchangeController(InstanceInformationService instanceInformationService) {
+    public ExchangeController(InstanceInformationService instanceInformationService, ExchangeRepository repository) {
         this.instanceInformationService = instanceInformationService;
+        this.repository = repository;
     }
-
 
     @GetMapping(value = "{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable BigDecimal amount, @PathVariable String from, @PathVariable String to) {
-        return new Exchange(
-                1L,
-                from,
-                to,
-                BigDecimal.ONE,
-                BigDecimal.TWO,
-                instanceInformationService.retrievePort()
-        );
+        Exchange exchange = repository.findByFromAndTo(from, to).orElseThrow(() -> new RuntimeException("Currency Unsupported"));
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+
+        exchange.setConvertedValue(convertedValue);
+        exchange.setEnvironment(instanceInformationService.retrievePort());
+
+        return exchange;
+
     }
 }
--- src/main/java/br/com/erudio/models/Exchange.java
+++ src/main/java/br/com/erudio/models/Exchange.java
@@ -1,5 +1,9 @@
 package br.com.erudio.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,15 +17,28 @@ import java.util.Objects;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity(name = "exchange")
 public class Exchange implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "from_currency")
     private String from;
+
+    @Column(name = "to_currency")
     private String to;
+
+    @Column(name = "conversion_factor")
     private BigDecimal conversionFactor;
+
+    @Transient //diz que a variavel deve ser ignorada no contexto do banco de dados
     private BigDecimal convertedValue;
+
+    @Transient
     private String environment;
 
 
